e2f9a4f021d78a9ebe1abf427b46d4078e7d69ee
insurecar <rostyslavshyian@gmail.com>
2021-08-03 08:42:07 +0300

    finished benefits and add file pricing.md

--- src/components/Benefits/Benefits.jsx
+++ src/components/Benefits/Benefits.jsx
@@ -3,64 +3,86 @@ import './Benefits.scss';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { useStaticQuery, graphql } from 'gatsby';
 import Button from '../UI/Button/Button';
+import { useI18next } from "gatsby-plugin-react-i18next"
 
 const Benefits = () => {
+
+  const { language } = useI18next()
+
   const {
-    allImageSharp: { nodes },
+    allMarkdownRemark: {
+      edges: [
+        {
+          node: { frontmatter },
+        },
+      ],
+    },
   } = useStaticQuery(graphql`
-        query Benefits {
-          allImageSharp(
-            filter: {
-              fluid: { originalName: { regex: "/.*(benefitsImg1|benefitsImg2|benefitsImg3|benefitsBackground).*/" } }
+    query Benefits {
+      allMarkdownRemark(
+        filter: {
+          fileAbsolutePath: { regex: "/benefits.md$/" }
+          frontmatter: { en: {} }
+        }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              jp {
+                anchor
+            title
+            subtitle
+            benefits_block {
+              image
+              title
+              text
             }
-          ) {
-            nodes {
-              gatsbyImageData
+            benefit_bottom_title
+            benefit_bottom_subtitle
+            button
+            background_bottom
+              }
+              en {
+                anchor
+            title
+            subtitle
+            benefits_block {
+              image
+              title
+              text
+            }
+            benefit_bottom_title
+            benefit_bottom_subtitle
+            button
+            background_bottom
+              }
             }
           }
         }
-      `);
-
-  const content = [
-    {
-      title: 'Reward more than just purchases',
-      text: 'With EasyPoints, you can reward customers for a number of actions outside of the cart, from liking your social media page to creating an account with your shop. Don’t just create loyal customers - create a loyal community!',
-      img: nodes[3].gatsbyImageData,
-    },
-    {
-      title: 'Point redemption without the redirect',
-      text: ' Loyalty is meant to reward, not frustrate. Most online loyalty programs utilize this clunky and disjointed setup, which diminishes the value of your program. Instead, EasyPoints focuses on the simplicity of point redemption directly from checkout. Save your customers the hassle and make it as simple as can be. ',
-      img: nodes[1].gatsbyImageData,
-    },
-    {
-      title: 'Don’t make it forgettable',
-      text: 'With EasyPoints, you can reward customers for a number of actions outside of the cart, from liking your social media page to creating an account with your shop. Don’t just create loyal customers — create a loyal community!',
-      img: nodes[2].gatsbyImageData,
-    },
-  ];
+      }
+    }
+  `)
 
   return (
     <div className="benefits" id = "benefits">
       <div className="container">
         <div className="benefits__header">
-          <p className="benefits__header-anchor" data-aos="fade-up"  data-aos-delay="150">Benefits</p>
+          <p className="benefits__header-anchor" data-aos="fade-up"  data-aos-delay="150">{frontmatter[language].anchor}</p>
           <h2 className="benefits__header-title" data-aos="fade-up" data-aos-delay="200">
-            Loyalty is essential for growth, but oftentimes hard to maintain
+            {frontmatter[language].title}
           </h2>
           <p className="benefits__header-text" data-aos="fade-up" data-aos-delay="250">
-            Creating an effective, engaging loyalty program is our goal at EasyPoints, and with over 10 different features, you’ll be sure to build the perfect program for both you and your customers.
+            {frontmatter[language].subtitle}
           </p>
         </div>
         <div className="benefits__main">
-          {content.map((elem) => (
+          {frontmatter[language].benefits_block.map((elem) => (
 
             <div className="benefits__main-block" key={elem.title + elem.text} >
-              {/* <div className="benefits__main-block-image" data-aos="zoom-out"  data-aos-delay={200}> */}
               <div className="benefits__main-block-image" data-aos="fade-up" data-aos-delay="250">
-
-                <GatsbyImage image={getImage(elem.img)} alt="graph" className = "benefits__main-block-image-picture" />
+                <img src={elem.image} alt="" className = "benefits__main-block-image-picture" />
               </div>
-              {/* <div className="benefits__main-block-text" data-aos="zoom-out"  data-aos-delay={400}> */}
               <div className="benefits__main-block-text" data-aos="fade-up" data-aos-delay="250">
                 <h3 className="benefits__main-block-text-title">{elem.title}</h3>
                 <p className="benefits__main-block-text-paragraph">{elem.text}</p>
@@ -71,21 +93,17 @@ const Benefits = () => {
         </div>
       </div>
       <div className="benefits__footer">
-        <GatsbyImage
-          image={getImage(nodes[0].gatsbyImageData)}
-          aria-hidden="true"
-          className="benefits__footer-background"
-          alt = 'background__footer'
-        />
+        <img src={frontmatter[language].background_bottom} className="benefits__footer-background" alt="" />
         <div className="container">
           <div className="benefits__footer-header">
             <h2 className="benefits__footer-header-title">
-              Create a compelling rewards experience and help your business grow
+              {frontmatter[language].benefit_bottom_title}
             </h2>
             <p className="benefits__footer-header-text">
-              Creating a loyalty program with EasyPoints is a simple but effective way to boost your customer retention strategy.
+            {frontmatter[language].benefit_bottom_subtitle}
+
             </p>
-            <Button type="secondary" text="Shopify App Store" />
+            <Button type="secondary" text = {frontmatter[language].button} />
           </div>
         </div>
       </div>
